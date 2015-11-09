2dcfc16c6d4bbe99467ec7897977dbe324c03e86
Jeff Fermon <jfermon22@gmail.com>
2015-11-08 22:48:09 -0700

    Initial Commit

---
+++ proj2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  proj2
+//
+//  Created by Jeff Fermon on 11/8/15.
+//  Copyright (c) 2015 Jeff Fermon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
